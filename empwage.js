b23e4a36b9eac51fe5b4ac35d307ae5cfb95c982
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-08 13:38:51 +0530

    [akhila]Refactor:Adding commits

--- empwage.js
+++ empwage.js
@@ -1,5 +1,6 @@
 
 //UC-2 PartTime  or FullTime
+//constants
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
@@ -9,14 +10,15 @@ let  empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
    switch (empCheck)
   {
+     //employee is present and parttime
     case IS_PART_TIME:
         empHrs = PART_TIME_HRS;
         break;
-
+    //employee is present and fulltime
     case IS_FULL_TIME:
        empHrs = FULL_TIME_HRS;
        break;
-
+     //employee is absent
     default:
       empHrs = 0;
    }
