32b4c4a52e787b2fd218b748aa641e1c63086af2
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-08 13:31:33 +0530

    [akhila]Refactor:Calculating emp wage

--- empwage.js
+++ empwage.js
@@ -1,11 +1,26 @@
-//UC-1  employee is present or absent
-const IS_PRESENT = 0
-let empcheck = Math.floor(Math.random() * 10) % 2;
-if (empcheck == IS_PRESENT)
-{
-console.log("employee is absent");
-}
-else
-{
-console.log("employee is present");
-}
+
+//UC-2 PartTime  or FullTime
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+let  empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+   switch (empCheck)
+  {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HRS;
+        break;
+
+    case IS_FULL_TIME:
+       empHrs = FULL_TIME_HRS;
+       break;
+
+    default:
+      empHrs = 0;
+   }
+   let empWage = empHrs * WAGE_PER_HR;
+   console.log( "emp wage :"+empWage);
+   
+   
